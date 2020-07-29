256ec4c0183ece9892399422d15cc4385929dfc8
jdwjdwjdw <jacobdeanwilson@gmail.com>
2020-07-28 20:18:35 -0700

    Updated shop page and shop redux to utilize redux-thunk

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -13,3 +13,5 @@ export const selectCollectionsForPreview = createSelector(
 export const selectCollection = memoize((collectionUrlParam) =>
 	createSelector([ selectCollections ], (collections) => (collections ? collections[collectionUrlParam] : null))
 );
+
+export const selectIsCollectionFetching = createSelector([ selectShop ], (shop) => shop.isFetching);
